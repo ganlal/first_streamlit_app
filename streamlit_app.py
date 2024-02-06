e84f3c1c34e2054766e99cbda688656c21d6754b
G <ganlal@yahoo.com.au>
2024-02-06 15:01:44 +0530

    Added mote items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Brakfast Menu')
+
+streamlit.text('Omega-3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hardboiled free-range egg')
